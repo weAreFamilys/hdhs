f82f06fbe214fda8064a36459cbfd981f40a7ac2
张昆 <zhangkun@szzt.com.cn>
2017-10-29 10:53:48 +0800

    edit user list

---
+++ hs_admin/src/main/java/com/hs/admin/constants/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.hs.admin.constants;
+
+/**
+ * @user :flyxk
+ * date :2017/10/29
+ */
+public class CommonConstant {
+
+    public static String KEY_BACKEND_USER = "KEY_BACKEND_USER";
+}
--- hs_admin/src/main/java/com/hs/admin/controller/UserController.java
+++ hs_admin/src/main/java/com/hs/admin/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.hs.admin.controller;
 
+import com.hs.admin.model.User;
+import com.hs.admin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @user :flyxk
@@ -11,8 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/list")
-    public String list() {
+    public String list(HttpServletRequest request) {
+        List<User> userList = userService.userList();
+        request.setAttribute("userList", userList);
         return "/user/list";
     }
 }
