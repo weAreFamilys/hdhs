22613b0d9f377601b54059e31e4cac2ac1474ffa
hopechen890428 <872987912@qq.com>
2017-10-31 17:50:38 +0800

    add sign

---
+++ pages/js/libs/eventUtil.js
@@ -0,0 +1,18 @@
+var EventUtil ={
+    addHandler : function(element,type,handler){
+           if(element.addEventListener){
+               element.addEventListener(type,handler,false)
+           }else if(element.attachEvent){
+               element.attachEvent("on" + type, handler);
+           }else{
+               element["on" + type] = handler;
+           }
+    },
+
+    getEvent : function(event){
+        return event ? event : window.event;
+    },
+    getTarget : function(event){
+        return event.target || event.srcElement;
+    }
+}
--- pages/js/libs/index.js
+++ pages/js/libs/index.js
@@ -1,3 +1,34 @@
 $(function () {
 
 });
+
+//登录
+function sendMsg(){
+    //按钮类型（注册、登录）
+    alert(btntype);
+    var name = $("#usrname").val();
+    alert("name is"+ name);
+    var pwd = $("#psw").val();
+    alert("pwd is" + pwd);
+    $.ajax({
+        type:"POST",
+        // url:"#",
+        data:{
+            username:name,
+            possword:pwd,
+        },
+        dataType:"json",
+
+        success:function(data){
+          console.log(data);
+            if (data && data.success) {
+                alert("success");
+            } else {
+                alert(data.msg);
+            }
+        },
+        error:function(data){
+            alert("error");
+        }
+    });
+}
